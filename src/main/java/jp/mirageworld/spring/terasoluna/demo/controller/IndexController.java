7d58b77e1eaacdc5229ea7f4aa5497f04d0481f6
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-04 09:08:25 +0900

    コメント調整、ログ出力調整

--- src/main/java/jp/mirageworld/spring/terasoluna/demo/controller/IndexController.java
+++ src/main/java/jp/mirageworld/spring/terasoluna/demo/controller/IndexController.java
@@ -13,22 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Handles requests for the application home page.
+ * application home page.
  */
 @Slf4j
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@GetMapping
 	@PostMapping
 	public String home(Locale locale, Model model) {
-		log.info("Welcome home! The client locale is {}.", locale);
+		log.info("client locale is {}.", locale);
+
 		LocalDateTime now = LocalDateTime.now();
 		model.addAttribute("serverTime", now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
 		return "welcome/home";
 	}
 
