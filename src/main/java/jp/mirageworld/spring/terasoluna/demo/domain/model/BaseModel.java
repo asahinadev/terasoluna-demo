a7091bc51e0d2c613a029ebb740122d660ccb4b1
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-04 10:09:55 +0900

    直列化設定

--- src/main/java/jp/mirageworld/spring/terasoluna/demo/domain/model/BaseModel.java
+++ src/main/java/jp/mirageworld/spring/terasoluna/demo/domain/model/BaseModel.java
@@ -1,12 +1,14 @@
 package jp.mirageworld.spring.terasoluna.demo.domain.model;
 
+import java.io.Serializable;
+
 /**
  * ベースモデル.
  *
  * @param <ID>
  *                 {@link javax.persistence.Id}
  */
-public interface BaseModel<ID> {
+public interface BaseModel<ID> extends Serializable {
 
 	ID getId();
 }
