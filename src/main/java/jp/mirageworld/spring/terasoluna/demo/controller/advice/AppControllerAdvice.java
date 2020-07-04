b69d303bb33d4206937d821ecd66f6bbeb415563
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-04 11:03:53 +0900

    空白文字を null に変更

---
+++ src/main/java/jp/mirageworld/spring/terasoluna/demo/controller/advice/AppControllerAdvice.java
@@ -0,0 +1,15 @@
+package jp.mirageworld.spring.terasoluna.demo.controller.advice;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class AppControllerAdvice {
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+}
